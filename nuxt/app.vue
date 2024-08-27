dfa3aa80104003d92191ada6729f5a01e39f17c7
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-08-27 14:02:08 -0400

    update app

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <SiteHeader v-if="route.name != 'index' "/>
+    <NuxtPage />
+    <SiteFooter/>
   </div>
 </template>
+
+<script setup>
+  const route = useRoute()
+  console.log('current name', route.name)
+</script>
