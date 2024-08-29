af3df943f604af3dc5c38ae0e8b5acf7f67e81a4
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-08-29 10:25:43 -0400

    update image

--- nuxt/components/homepage/Hero.vue
+++ nuxt/components/homepage/Hero.vue
@@ -1,8 +1,13 @@
 <template>
-  <section class="relative px-4 pt-12 w-full h-[80svh] bg-[radial-gradient(80.40%_89.55%_at_0%_6.29%,rgba(186,0,255,1)_0%,rgba(0,0,0,0)_100%)]">
+  <section class="relative px-4 pt-12 w-full h-[80svh]">
     <div class="ml-col1 pl-4">
-      <IconsLogo class="h-24 w-auto" />
-      <p class="font-hatton text-hatton-m mt-8 w-[20rem]">
+      <IconsLogo class="h-24 z-10 relative w-auto" />
+      <img
+        src="/pngs/homepage_gradient.png"
+        alt="Hero Image"
+        class="absolute z-0 top-0 left-0 w-full h-full object-contain object-left"
+      />
+      <p class="font-hatton z-10 relative text-hatton-m mt-8 w-[20rem]">
         A multi-city art campaign to dream, envision, and create a <span class="text-navigation-highlight">future where all youth are free, safe, and thriving.</span>
       </p>
     </div>
