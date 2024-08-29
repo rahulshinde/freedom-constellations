8dcb2e4ae6e94506a04e4a037a1ef02b83378624
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-08-29 12:35:23 -0400

    update homepage links

--- nuxt/components/block/LocationLinks.vue
+++ nuxt/components/block/LocationLinks.vue
@@ -1,23 +1,28 @@
 <template>
-  <section class="mx-8 font-hatton text-hatton-l grid md:grid-cols-5 gap-8 mt-16 pb-16">
+  <section class="mx-8 font-hatton text-hatton-l grid md:grid-cols-5 gap-4 mt-16 pb-16">
     <NuxtLink class="hover:text-navigation-highlight transition-colors" to="/virginia">
       Virginia
+      <img src="/homepage/links/virginia.jpg" class="w-full mt-4"/>
     </NuxtLink>
 
     <NuxtLink class="hover:text-navigation-highlight transition-colors" to="/illinois">
       Illinois
+      <img src="/homepage/links/illinois.jpg" class="w-full mt-4"/>
     </NuxtLink>
 
     <NuxtLink class="hover:text-navigation-highlight transition-colors" to="/pennsylvania">
       Pennsylvania
+      <img src="/homepage/links/pennsylvania.jpg" class="w-full mt-4"/>
     </NuxtLink>
 
     <NuxtLink class="hover:text-navigation-highlight transition-colors" to="/new-jersey">
       New Jersey
+      <img src="/homepage/links/jersey.jpg" class="w-full mt-4"/>
     </NuxtLink>
 
     <NuxtLink class="hover:text-navigation-highlight transition-colors" to="/minnesota">
       Minnesota
+      <img src="/homepage/links/minnesota.jpg" class="w-full mt-4"/>
     </NuxtLink>
   </section>
 </template>
