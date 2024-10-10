9ac9e531b1982f98720686d3c0665fbd4329d1a7
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-10 16:18:41 -0400

    adding pngs to virginia index

--- nuxt/pages/virginia/index.vue
+++ nuxt/pages/virginia/index.vue
@@ -1,8 +1,10 @@
 <template>
   <main class="w-full overflow-hidden">
-    <BlockTextHeading class="relative z-20" text="Virginia" />
-    <BlockTextBody class="!mt-0 relative z-20" text="As Richmond dismantled monuments to their painful and devastating legacy of slavery and the confederacy, Freedom Constellations (2020-2022) represented a new, interactive series of monumental public art projects that asked young people to illustrate a road map toward freedom for us all." />
-
+    <div class="relative">
+      <BlockTextHeading class="relative z-20" text="Virginia" />
+      <BlockTextBody class="!mt-0 relative z-20" text="As Richmond dismantled monuments to their painful and devastating legacy of slavery and the confederacy, Freedom Constellations (2020-2022) represented a new, interactive series of monumental public art projects that asked young people to illustrate a road map toward freedom for us all." />
+      <img class="absolute z-[1] -top-[0vw] left-0 w-[20vw]" src="/zine-assets/bluecrown2.png" />
+    </div>
     <section 
       class="
         bg-black relative z-10 pb-16
@@ -17,6 +19,9 @@
 
 
       <div class="relative">
+        <img class="absolute -z-[1] -top-[15vw] right-[10vw] w-[30vw]" src="/zine-assets/threecircles1.png" />
+        <img class="absolute -z-[1] top-[5vw] right-[10vw] w-[10vw]" src="/zine-assets/orange star.png" />
+
         <IndexLink
           class="relative z-10"
           text="AR Animations Over City Hall"
@@ -27,6 +32,8 @@
       </div>
 
       <div class="relative">
+        <img class="absolute -z-[1] -top-[15vw] left-[20vw] w-[35vw]" src="/zine-assets/planet1.png" />
+        <img class="absolute -z-[1] top-[5vw] left-[20vw] w-[18vw]" src="/zine-assets/greenstar.png" />
         <IndexLink
         class="relative z-10"
         text="A Mural That Comes Alive"
@@ -45,6 +52,7 @@
           img="/virginia/index/GRTC_FreedomConstellations_2small.jpg"
           color="var(--projects)"
         />
+        <img class="absolute -z-[1] top-[0vw] right-[0vw] w-[30vw]" src="/zine-assets/orangestar1.png" />
       </div>
 
       <div class="relative">
@@ -56,16 +64,19 @@
         color="var(--projects)"
         :reverse="true"
         />
+        <img class="absolute -z-[1] top-[-9rem] left-[-4rem] w-[30vw]" src="/zine-assets/zine-assets_0004_Layer-8.png" />
       </div>
 
-      <div class="relative overflow-hidden">
-      <BlockTextBody
-        class="pb-16"
-        text="As the first in the series of Freedom Constellations campaigns, Richmond showcased many powerful models to build from as we moved to work with other state campaigns, but also areas where we could make those versions even more powerful. 
+      <div class="relative">
+        <BlockTextBody
+          class="pb-16"
+          text="As the first in the series of Freedom Constellations campaigns, Richmond showcased many powerful models to build from as we moved to work with other state campaigns, but also areas where we could make those versions even more powerful. 
 
 As you move on to other states, you’ll see how the #NoKidsInPrison exhibition, billboards, and other strategies were added to further engage massive and strategic audiences while providing a space for events, organizing, and convenings. Huge thanks to everyone who made Richmond’s campaigns possible. 
 "
-      />
+        />
+        <img class="absolute -z-[1] rotate-45 bottom-[2rem] right-[4rem] w-[15vw]" src="/zine-assets/zine-assets_0014_Layer-4.png" />
+      
       </div>
     </section>
   </main>
