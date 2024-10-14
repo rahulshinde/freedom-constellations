ff9a743f529f8e7592ef663e292c37880e735d9a
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-14 11:18:29 -0400

    update minnesota landing page

--- nuxt/pages/minnesota/index.vue
+++ nuxt/pages/minnesota/index.vue
@@ -2,13 +2,14 @@
   <main>
     <BlockTextHeading class="relative z-20" text="Minnesota" />
     <BlockTextBody class="!mt-0 relative z-20" text="In the wake of the murder of George Floyd, Minnesota galvanized uprisings across the country inspiring a national reckoning with American policing. Schools across the Twin Cities ended their contracts with police but the End Youth Prisons MN campaign wanted to tell a deeper story about the underpinnings of Minnesota history, one punctuated by corporate racism and the genocide of Indigenous people.  In Minnesota, mapping a future of youth liberation meant unpacking hidden stories and imagining transformation led by youth vision." />
-
+    
     <section 
     class="
     bg-black relative z-10
     before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black from-black-trans before:-top-[20svh] before:left-0 before:z-0
     "
     >
+      <BlockImageFullWidth class="z-10 relative" img="/minnesota/index/NKIPMNpeople10.jpg" alt="NKIP Philly 21" />
       <BlockTextBody text="Use the sections below to experience and learn more about each project that took place in Michigan." />
 
       <div class="relative">
