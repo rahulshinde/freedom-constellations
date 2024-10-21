e7cd3f9f7f704942110506021ef08dbea0911a71
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-21 13:05:28 -0400

    starting content entry for jersey + pennsylvania

---
+++ nuxt/pages/new-jersey/coi.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+
+    <BlockProjectHeader
+      class="relative z-20"
+      title="New Jersey"
+      backLink="/new-jersey"
+      subtitle="Put Youth in Power Not Prison"
+      location=""
+      introText="The New Jersey cohort began their process as part of our Cultural Organizing Institute, developed in collaboration with Youth First as an effort to bring together campaigns to learn about creative strategy and cultural organizing during pandemic. Together with organizers from Connecticut and Maine, teams from Newark and Camden started their project through a series of virtual workshops."
+      date=""
+    />
+
+    <section 
+      class="
+        bg-black relative z-10 pb-16
+        before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black"
+    >
+      <BlockTextBody
+        text="While the governor of New Jersey was preparing to make recommendations to build 3 new youth prisons, spending over $600k per year on imprisoning each young person, the New Jersey cohort came together around a campaign illustrating youth leadership and power. The jumpsuit takeback project grew out of the messaging “Put Youth in Power not Prison.”  In a state where Black youth are 18x more likely to be incarcerated than their white peers, the team worked to create iconic images of BIPOC youth glowing with messaging and symbolism of their values and demands for NJ to invest in their dreams.  
+
+Inspired by the iconography of The Black Panthers, Young Lords and Afro-futurism, the team settled on a color scheme that felt youthful, futurist and royal. They chose to play with crowns, butterflies and breaking chains. Nodding to the revolutionary history of the beret, they decided to nod to the lineage by including a graduation cap. The top line messaging that led this body of art included: 
+
+• Put Youth In Power Not Prison
+• Fund the Future
+• Uncuff the Future
+• Healing Not Harm
+• Destroy Detainment
+
+Once the jumpsuits and caps were created, Performing Statistics and Project for Empty Space facilitated photoshoots with organizers wearing the garments in Newark and Camden. Portraits of the organizers were collaged into posters, banners and billboard designs. Giant banners were printed and hung in downtown Newark. During the #NoKidsInPrison exhibit in neighboring Philadelphia, billboards filled the streets of Camden. The woodcut sculptures are slated to be installed at a local organization in Camden. Art went up on the only billboard on the way to the largest youth prison in New Jersey, making sure that everyone driving there had to hear the demand of these youth leaders, “Put Youth in Power, Not Prison!”
+"
+      />
+
+    </section> 
+  </main>  
+</template>
+
+<script setup>
+</script>
---
+++ nuxt/pages/pennsylvania/nkip.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+
+    <BlockProjectHeader
+      class="relative z-20"
+      title="Pennsylvania"
+      backLink="/pennsylvania"
+      subtitle="#NoKidsInPrison: Cherry Street Pier Exhibition"
+      location="121 N Christopher Columbus Blvd, Philadelphia, PA."
+      introText="Organized by Performing Statistics, this interactive exhibition took viewers on an immersive journey from the history of youth incarceration, to the present-day impact on youth and their families, and ended in a future space world where all youth are free. Filled with virtual reality, interactive art installations, and eight years of art made in collaboration with youth leaders nationwide, the exhibition engaged thousands of viewers and helped support local campaigns to keep youth free."
+      date=" May 20 – Jun 11, 2023"
+      :onThisPage="[
+        ['Exhibition Data', 'data'],
+        ['Programming', 'programming']
+      ]"
+    />
+
+    <section 
+      class="
+        bg-black relative z-10 pb-16
+        before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black from-black-trans before:-top-[20svh] before:left-0 before:z-0"
+    >
+      
+
+    </section> 
+  </main>  
+</template>
+
+<script setup>
+</script>
