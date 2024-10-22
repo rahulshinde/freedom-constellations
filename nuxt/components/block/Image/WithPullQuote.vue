d6a77cb2d1d17f7da9e1408ed86c9851d5e29a76
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-22 14:48:14 -0400

    adding mural content

--- nuxt/components/block/Image/WithPullQuote.vue
+++ nuxt/components/block/Image/WithPullQuote.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="flex mx-4 mt-8 items-stretch">
+  <div class="flex mx-4 mt-8 items-center"
+    :class="reverse ? 'flex-row-reverse' : 'flex-row'"
+    >
     <img class="w-col6 mx-4 max-h-[80svh] object-contain" :src="img">
     <div class="w-col6 flex relative mx-4">
-      <p class="text-white font-hatton self-center text-hatton-l w-[70%]">{{ quote }}</p>
+      <p 
+        class="flex flex-col w-[70%]"
+        :class="[
+          reverse ? 'ml-[30%]' : '',
+          textColorClass(color)
+        ]"
+      >
+        <span class="quote relative pl-3 font-hatton self-center text-hatton-l">{{ quote }}”</span>
+        <span class='ml-8 mt-2 text-runde-m'>—{{ attribution }}</span>
+      </p>
 
       <div
         class="flex absolute bottom-0 items-end"
@@ -19,6 +30,32 @@
   const props = defineProps({
     img: String,
     quote: String,
-    includeArLink: Boolean
+    attribution: String,
+    includeArLink: Boolean,
+    color: String,
+    reverse: Boolean
+  });
+
+  const textColorClass = ((color) => {
+    switch (color) {
+      case 'var(--nkip)':
+        return 'text-nkip';
+      case 'var(--billboards)':
+        return 'text-billboards';
+      case 'var(--projects)':
+        return 'text-projects';
+      case 'var(--programs)':
+        return 'text-programs';
+      case 'var(--bg-highlight)':
+        return 'text-bg-highlight';
+    }
   });
 </script>
+
+<style>
+  .quote:before{
+    content: '“';
+    position: absolute;
+    left: -0.25rem
+  }
+</style>
--- nuxt/pages/virginia/mural.vue
+++ nuxt/pages/virginia/mural.vue
@@ -69,28 +69,116 @@ By collaborating with the youth coders at CODE VA to do the programming for the 
         text="The mural stayed on display for over a year and was directly across the street from the police department. Positioned at 3rd and Broad st (one of the busiest intersections in the city), the mural received a feature article in the <a href='https://richmond.com/entertainment/art/on-the-wall-across-from-vcu-police-department-advocates-unveil-a-mural-that-imagines-a/article_0dc739f2-83c2-5318-b6c0-67fcf41ceb2f.html' target='_blank'>city’s largest newspaper</a> and was viewed by thousands of people."
       />
 
+      <div class="mx-4">
+        <div class="flex mt-16 mb-16 justify-between mx-4">
+          <img
+            class="w-1/3 mr-4 h-[35vw] object-cover"
+            src="/virginia/mural/Iyana_Poster.jpg"
+          >
+          <img
+            class="w-2/3 object-cover ml-4 h-[35vw]"
+            src="/virginia/mural/kidaya_poster.jpg"
+          >
+        </div>
+      </div>
+
+      <BlockImageWithPullQuote 
+        img="/virginia/mural/kayla_poster.jpg"
+        quote="I could be a hero to some kids and they could be like us."
+        attribution="Kayla"
+        color="var(--projects)"
+      />
+
+      <BlockImageWithPullQuote 
+        img="/virginia/mural/khai_poster.jpg"
+        quote="I feel like public art is going to always be there. It can affect the future. When the police officers look out their window and see the mural I hope they feel challenged to make a change."
+        attribution="Khai, 17"
+        color="var(--projects)"
+        :reverse="true"
+      />
+
+      <BlockTextHeading
+        text="New world radio"
+      />
+
       <div class="mx-4 mt-8">
         <div class="ml-col3 mr-col3">
           <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1161880087&color=%23ffcd00&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
         </div>
       </div>
 
-      <BlockTextPullQuote
-        text="I could be a hero to some kids and they could be like us."
-        attribution="Kayla"
-        :reverse="true"
-        color="var(--projects)"
+      <BlockTextBody
+        text="New World Radio is a radio broadcast from a future world where no youth are locked up. The audio piece was produced for the interactive mural; Freedom Constellations: Dreaming of a World Without Youth Prisons in Richmond, VA. The mural imagines a world where no youth are locked up and people can hear the audio as they scan their phones over the artwork."
       />
 
-      <BlockTextPullQuote
-        text="I feel like public art is going to always be there. It can affect the future. When the police officers look out their window and see the mural I hope they feel challenged to make a change."
-        attribution="Khai, 17"
-        color="var(--projects)"
+      <BlockTextHeading
+        text="Credits"
       />
 
+      <BlockCredits
+        :credits="credits"
+      />
+      <BlockCredits
+        title="New World Radio"
+        :credits="nwr_credits"
+      />
     </section>
   </main>
 </template>
 
 <script setup>
+  const nwr_credits = [
+    {
+      title: 'Direction and Editing',
+      text: 'Nicki Stein in collaboration with Iyana, Kayla, Khai, Kidaya, and Ta’Dreama'
+    },
+    {
+      title: 'Additional interviews',
+      text: 'Naila Lyles and Mark Strandquist'
+    },
+    {
+      title: 'We Dream of a World Where No Youth Are Locked UP',
+      text: 'written by Iyana, Kayla, Khai, Kidaya, and Ta’Dreama'
+    }
+  ]
+
+  const credits = [
+    {
+      title: 'Partners',
+      text: 'CodeVA and RISE for Youth'
+    },
+    {
+      title: 'Youth muralists',
+      text: 'Iyana, Kayla, Khai, Kidaya, Ta’Dreama'
+    },
+    {
+      title: 'Lead muralists',
+      text: 'Mark Strandquist and Kate DeCiccio'
+    },
+    {
+      title: 'Creative assistant',
+      text: 'Naila Lyles-Elcock'
+    },
+    {
+      title: 'Mural assistant',
+      text: 'Emily Ellingsworth'
+    },
+    {
+      title: 'Audio/Video Teaching Artists',
+      text: 'Jasmine Leeward and Nicki Stein'
+    },
+    {
+      title: 'CodeVA Eureka Workshop instructors',
+      text: 'Zach Mulcahey and Natasha Kovacs'
+    },
+    {
+      title: 'Youth Technologists',
+      text: 'I.Rodriguez and Jakson'
+    },
+    {
+      title: '',
+      text: 'This mural was supported, in part, by the Richmond Memorial Health Foundation and the Robins Foundation.'
+    }
+    
+  ]
 </script>
