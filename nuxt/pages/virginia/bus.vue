ad4a859c48ff5b2793b73c8e4146b8d8e6285893
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-24 14:46:34 -0400

    publish bus page

--- nuxt/pages/virginia/bus.vue
+++ nuxt/pages/virginia/bus.vue
@@ -16,16 +16,53 @@
           bg-black relative z-10 pb-16
           before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black from-black-trans before:-top-[20svh] before:left-0 before:z-0"
       >
+      <BlockImageFullWidthSlideshow
+        class="mt-8"
+        :images="[
+          {
+            src: '/virginia/bus/GRTC_FreedomConstellations_1_small.jpg'
+          },
+          {
+            src: '/virginia/bus/GRTC_FreedomConstellations_2small.jpg'
+          },
+          {
+            src: '/virginia/bus/GRTC_FreedomConstellations_13small.jpg'
+          }
+        ]"
+      />
       <BlockTextBody
         text="The unveiling of the bus happened directly below the monumental portraits on City Hall. It was super inspiring to think of City Hall as the central star of Freedom Constellations, and the bus as the vehicle for bringing these message of hope and justice to people and communities across the city. 
 
-  ***This was the first ever collaboration for the GRTC Transit System with nonprofit and philanthropic partners and it's their hope that it kicks off a series of bus wraps featuring different community groups who are important to GRTC ridership!
+***This was the first ever collaboration for the GRTC Transit System with nonprofit and philanthropic partners and it's their hope that it kicks off a series of bus wraps featuring different community groups who are important to GRTC ridership!
 
-  Huge thanks to CultureWorks Richmond and GRTC for making this possible! And to Fuji Film for lending us a camera powerful enough to make these monumental portraits pop!"
+Huge thanks to CultureWorks Richmond and GRTC for making this possible! And to Fuji Film for lending us a camera powerful enough to make these monumental portraits pop!"
       />
+      <BlockFullBleed
+          video="https://player.vimeo.com/progressive_redirect/playback/1023014958/rendition/480p/file.mp4?loc=external&signature=4f55b1005ad0a6aec968ec9dc1698bd014f051a41182476a2daef7d37d38a3ad"/>
+      <BlockTextHeading text="Credits"/>
+      <BlockCredits 
+          :credits="credits"/>
     </section>
   </main>
 </template>
 
 <script setup>
+  const credits = [
+    {
+      title: 'Youth leaders featured',
+      text: 'Ta’Dreama, Clyde, Seth, Kiara, Iyana, and Denise (you all are amazing!!!)'
+    },
+    {
+      title: 'Photography and art direction',
+      text: 'Mark Strandquist '
+    },
+    {
+      title: 'Background shapes',
+      text: 'Kate DeCiccio, Kah Yangni, Amir Khadar, and Jay Krouse '
+    },
+    {
+      title: 'Layout and design for bus wrap',
+      text: 'Jay Krouse'
+    }
+  ]
 </script>
