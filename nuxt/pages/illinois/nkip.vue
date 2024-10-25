a872f0221d6373396bdd6b70b3c5074004da15f3
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-25 11:49:03 -0400

    nkip starter file

---
+++ nuxt/pages/illinois/nkip.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <BlockProjectHeader
+      class="relative z-20"
+      title="Illinois"
+      backLink="/illinois"
+      subtitle="No Kids in Prison Exhibition"
+      location="Chicago, Illinois"
+      introText="Organized by Performing Statistics, #NoKidsInPrison Chicago featured collaborations with The Final 5 campaign and Circles and Ciphers. The exhibition immersed viewers into interactive installations about the history of youth incarceration, the state of youth incarceration today and what is needed to build a world where all youth are free. Youth voices from local incarcerated young people were included through an audio collaboration between Final Five and youth inside. SkyArts contributed paintings created by local incarcerated youth. Circles and Ciphers created a music video to drop their new song, “No Kids In Prison” a ballad that illustrates a prison free Chicago that centers youth leadership."
+      date=""
+      :onThisPage="[
+      ]"
+    />
+  </main>
+</template>
+
+<script setup>
+</script>
