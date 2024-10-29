d388fe558be82d2d2aacb248cdacd4e1459974a1
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-29 13:14:29 -0400

    update billboards image

--- nuxt/pages/illinois/billboards.vue
+++ nuxt/pages/illinois/billboards.vue
@@ -21,10 +21,51 @@
         before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black from-black-trans before:-top-[20svh] before:left-0 before:z-0
       "
     > 
+      <BlockImageFullWidthSlideshow
+        class="mt-8"
+        :images="[
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards3.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards4.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards12.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards16.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards22.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards1.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards34.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards8.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards28.jpg',
+          },
+          {
+            src: '/illinois/billboards/NKIPChicagoBillboards37.jpg',
+          }
+        ]"
+      />
       <BlockTextBody
         text="Youth shared that they wanted the billboards to convey power, motivation, and a feeling of bold imagination and joy. They wanted to be represented in ways that express their passion, determination and that they deserve a seat at the table. They were hoping to see youth playing, broken chains, sun and POC being happy. 
 
 The Performing Statistics team facilitated a series of workshops that allowed organizers to share their ideas with artists and give them feedback on the direction of their designs. Using input from the young people, the artists created ten billboard designs that were featured throughout Chicago and in Springfield where legislators are making decisions about youth freedom in IL. "
+      />
+      <BlockTextHeading
+        text="Credits"
+      />
+      <BlockCredits
+          :credits="credits"
       />
       <BlockProjectFooterLinks
           heading="More from Illinois"
