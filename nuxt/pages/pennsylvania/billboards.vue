6f6f9b0f620682163b6b2c3aabd3c85ce72781c6
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-30 16:04:56 -0400

    billboards

---
+++ nuxt/pages/pennsylvania/billboards.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <BlockProjectHeader
+      class="relative z-20"
+      title="Pennsylvania"
+      backLink="/pennsylvania"
+      subtitle="Billboards: Transforming advertising space into visions of a more free future"
+      location="Philadelphia, PA"
+      introText="Can art create a map toward a more free future? Freedom Constellations worked with local youth leaders to use multiple forms of public art to guide us toward that just vision."
+      date="January 2023"
+      :onThisPage="[
+      ]"
+    />
+
+    <section 
+      class="
+        bg-black relative z-10 pb-16
+        before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black from-black-trans before:-top-[20svh] before:left-0 before:z-0
+      "
+    >
+      
+    </section>
+  </main>
+</template>
+
+<script setup>
+</script>
