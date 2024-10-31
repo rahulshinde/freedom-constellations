57de35acfece2701b900cc89c356936da6371e71
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-10-31 12:53:14 -0400

    mobile footer updates

--- nuxt/components/global/SiteFooter.vue
+++ nuxt/components/global/SiteFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="w-full max-w-full bg-black relative overflow-hidden pb-8 transition-opacity duration-[0.3s]">
     <img src="/pngs/squiggle.png" alt="Squiggle" class="max-w-none h-auto top-0 relative -left-2 -right-2" />
-    <div class="flex flex-wrap lg:flex-nowrap md:justify-center lg:justify-between items-start mr-4">
+    <div class="flex flex-wrap lg:flex-nowrap justify-center lg:justify-between items-start mr-4">
       <div class="mb-8 mx-0 md:mx-4 lg:justify-between flex flex-wrap md:flex-nowrap lg:flex-wrap md:w-full lg:w-col4">
         <NuxtLink to="/" class="flex md:w-1/3 lg:w-auto w-full mx-4 items-start">
           <IconsLogo class="block w-full h-16 md:h-12 md:w-auto" />
         </NuxtLink>
-        <IconsDream class="dream absolute lg:static left-[15rem] md:left-auto md:right-[13%] top-[30rem] md:top-24 w-[45%] md:w-[12rem] lg:w-2/3 rotate-[0deg] -mt-8 md:mt-4 opacity-75 md:-rotate-[7deg] h-auto md:mx-4"/>
+        <IconsDream class="dream absolute lg:static left-1/2 translate-x-[-50%] md:translate-x-0 md:left-auto md:right-[13%] top-auto bottom-4 md:bottom-auto md:top-24 w-[80%] md:w-[12rem] lg:w-2/3 rotate-[0deg] -mt-8 md:mt-4 opacity-75 md:-rotate-[7deg] h-auto md:mx-4"/>
       </div>
       <div class="mx-2 md:mx-4 flex lg:mt-8 w-full md:w-col6 lg:w-auto">
         <div class="w-1/2 md:w-[10rem] flex flex-col mx-2 lg:mx-4">
@@ -59,7 +59,7 @@
           </NuxtLink>
         </div>
       </div>
-      <div class="flex flex-col md:w-col6 lg:w-auto mt-8 md:-mt-4 md:mr-4 lg:mr-0 md:mt-4 items-center">
+      <div class="flex flex-col mb-32 md:w-col6 lg:w-auto mt-4 md:-mt-4 md:mr-4 lg:mr-0 md:mt-4 items-center">
         <AbolitionButton :large="true" class="self-center"/>
         <div class="flex items-center mt-4">
           <span class="border-[2px] rounded-[50%] font-bold text-[0.6rem] px-[0.4rem] py-[0.1rem]">?</span>
