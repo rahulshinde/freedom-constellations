3af204892b6858a3ffe9701feede1b54fdbfbac2
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-11-01 16:12:21 -0400

    mural text sections

--- nuxt/pages/new-jersey/mural.vue
+++ nuxt/pages/new-jersey/mural.vue
@@ -1,6 +1,24 @@
 <template>
   <main>
-    
+    <BlockProjectHeader
+      class="relative z-20"
+      title="New Jersey"
+      backLink="/new-jersey"
+      subtitle="Wheat Paste Mural: A mobile wheat paste mural featuring larger than life youth organizer icons!"
+      location="Camden, New Jersey"
+      introText="As cultural organizers working to reach broad audiences with imagery that affirms youth leadership, we’re always imagining how to access different spaces. With so many powerful portraits of the New Jersey team, the youth leaders were able to utilize several strategies to flood New Jersey with their vision of leadership. Organizers worked with Performing Statistics to cut out and paint huge wood cut outs and then wheatpaste giant print outs of their portraits onto the wood. The team held a photoshoot with the pieces in Camden and then they went on the road as part of #NoKidsInPrison."
+      date="2022"
+    />
+
+    <section 
+      class="
+        bg-black relative z-10 pb-16
+        before:content-[''] before:absolute before:w-full before:h-[20svh] before:bg-gradient-to-b to-black from-black-trans before:-top-[20svh] before:left-0 before:z-0"
+    >
+      <BlockTextBody
+        text="The beauty of this strategy is that the pieces are sturdy, moveable and stand alone. They are bold gorgeous portraits when inside and tough enough to stand the weather when installed outside. With every iteration of a project, we are doing our best to teach young people about materials and skills that they can apply to future projects. The mobile wheat paste mural sculptures will be installed in Camden so they can continue to model youth leadership and add youth energy and power to wherever they live."
+      />
+    </section>
   </main>
 </template>
 
