9ecb0f37056600f492a02c67bab4c96ff28864f7
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-11-08 12:12:55 -0500

    update bullet styling + coi text content

--- nuxt/components/block/Text/Bullet.vue
+++ nuxt/components/block/Text/Bullet.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="md:mx-4">
     <div class="flex flex-wrap md:px-12 md:flex-nowrap items-baseline mx-4 md:mx-col2 lg:mx-col3 text-white text-runde-m">
-      <IconsAstrix fill="var(--navigation-highlight)" class="w-6 md:w-col2 h-6 md:h-12 relative top-4 md:mr-4" />
-      <h2 class="top-2 md:top-0 relative md:w-col3 ml-4 mr-4 text-runde-s md:text-runde-m md:basis-col3 font-semibold">{{ props.title }}</h2>
-      <p class="pl-10 mt-4 md:mt-0 md:w-col10 text-runde-xs md:text-runde-m">{{ props.text }}</p>
+      <IconsAstrix fill="var(--navigation-highlight)" class="w-6 md:w-col2 h-6 md:h-8 relative top-3 md:mr-4" />
+      <h2 class="top-2 md:top-0 relative w md:w-col3 ml-4 mr-4 text-runde-s md:text-runde-s md:basis-col3 font-semibold">{{ props.title }}</h2>
+      <p class="pl-10 w-full mt-4 md:mt-0 md:w-col10 text-runde-xs md:text-runde-s">{{ props.text }}</p>
     </div>
   </div>
 </template>
--- nuxt/pages/pennsylvania/coi.vue
+++ nuxt/pages/pennsylvania/coi.vue
@@ -25,6 +25,55 @@
         video="https://player.vimeo.com/progressive_redirect/playback/1027747781/rendition/1080p/file.mp4?loc=external&signature=e69dca889dc573a9dcddebfd344a3680ab15402068daf5e6d91ba5b8575d70d3"
       />
 
+      <BlockTextBody
+        text="The goals of the strategy for the CNC organizers were:"
+      />
+
+      <BlockTextBulletList
+        :list="[
+          'Create a spectacle so whether Care Not Control is at a press conference or performing their album, they look like a coordinated team with clear visible messaging about what they represent and their demands.',
+          'The art must carry Care Not Control messaging and be able to be rolled out in many different locations.',
+          'Assets created can be used in future creative workshops with more young people. Care Not Control identified creative activity as one way that new organizers enjoy coming together. They wanted the designs they created to be able to be used to make more powerful swag with new campaign members.'
+        ]"
+      />
+
+      <BlockTextBody
+        text="The group decided that jackets and a series of beautiful signage would successfully meet their goals.
+
+From the Care Not Control Principles, a set of concrete demands to support young people to keep them free, we worked together to create a visual language that the organizers felt represents the spirit of liberation they are working to create. From being cuffed, to having fingerprints taken, they lifted up how much hands are involved through the process of being arrested and booked. The group decided they wanted part of the imagery to express taking back these violations, “Let’s take our fingerprints back!” The team landed on stars, megaphones, people power fists & fingerprints, broken chains, keys and arrows to use to begin to build their spectacle.
+
+Performing Statistics presented 3 directions alongside possible ways to alter each garment.  
+“Which garment best tells your story?”"
+      />
+
+      <BlockTextBullet
+        title="The Denim Jacket"
+        text="An American classic to take hold of your moment in history."
+      />
+      <BlockTextBullet
+        title="The Flight/Bomber Jacket"
+        text="A  symbol of flight and subversive change-making to steer us toward a new reality."
+      />
+      <BlockTextBullet
+        title="Construction/Workwear"
+        text="Garments that signal construction to flag you’re building a new future?"
+      />
+
+      <BlockTextBody
+        text="The majority of the team decided to work with bomber jackets and two people chose denim jackets. Their thinking was that they could represent present day ambassadors who were supporting to lead toward the more futuristic vision of their peers.
+        
+Once PS had all of this information, we presented the creative brief to Amir Khadar and Katie Kaplan. Amir and Katie ran with the ideas and worked through how to engineer all of the ideas into signage and assets for altering bomber jackets that the organizers could work with to make a series of similar but individual jackets. Amir took the lead designing text, pins, patches, vinyl and the overall aesthetic vision. Katie worked on figuring out interesting signage possibilities and strategized some of the more technical textile components. CNC wanted to work with messaging that aligned with their current work getting 5 bills passed:"
+      />
+
+      <BlockTextBulletList
+        :list="[
+          'End Direct File',
+          'End Fines & Fees',
+          'Raise the Age'
+        ]"
+      />
+
+
       <BlockTextHeading
         text="Credits"
       />
