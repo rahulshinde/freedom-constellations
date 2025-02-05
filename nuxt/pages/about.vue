0c99b99790488537faaf67ca27ac31e9ab5c3f41
Rahul S. Shinde <rsubhashshinde@gmail.com>
2025-02-05 13:06:26 -0500

    update about page

--- nuxt/pages/about.vue
+++ nuxt/pages/about.vue
@@ -11,25 +11,40 @@
       <BlockImageGallery
         :images="['/about/NKIPChicago1.jpg', '/about/MNBillboards30.jpg']"
       />
-      <div class="mt-8 mx-4">
-        <p class="ml-col3 mr-col3 text-white text-runde-xl">
+      <div class="mt-8 mx-12">
+        <p class="px-8 ml-col3 mr-col3 text-white text-runde-xl">
           This website is an archive of the Freedom Constellations campaigns produced by Performing Statistics and their partners between 2020 and 2024.
         </p>
-        <p class='ml-col3 mr-col3 text-white text-runde-m mt-8'>
+        <p class='px-8 ml-col3 mr-col3 text-white text-runde-m mt-8'>
           For more information about Performing Statistics check out their ongoing work <a href="https://www.performingstatistics.org/" target='_blank' class="underline hover:text-navigation-highlight">HERE</a>.
         </p>
-        <p class='ml-col3 mr-col3 text-white text-runde-m mt-4'>
+        <p class='px-8 ml-col3 mr-col3 text-white text-runde-m mt-4'>
           Interested in more information about the Freedom Constellations campaigns? Please fill out <a href='#' target='_blank' class="underline hover:text-navigation-highlight">this form</a> we’ll get right back to you; 
         </p>
-        <p class='ml-col3 mr-col3 text-white text-runde-m mt-4'>
+        <p class='px-8 ml-col3 mr-col3 text-white text-runde-m mt-4'>
           From 2021 - 2024, Performing Statistics was co-directed by Kate DeCiccio, J Slater, and Mark Strandquist. 
         </p>
-        <p class='ml-col3 mr-col3 text-white text-runde-m mt-4'>
+        <p class='px-8 ml-col3 mr-col3 text-white text-runde-m mt-4'>
           Endless thanks to all our partners and to all the brave and brilliant youth leaders who shared their voices, dreams and demands for a more just world. 
         </p>
       </div>
-      <div class="mt-12 pb-8 mx-4">
-        <div class="ml-col3 mr-col3 columns-3 gap-4">
+
+      <BlockTextHeading class="mt-8" text="Want to see more powerful art??" />
+
+      <div class="mt-8 mx-col3 px-10 flex">
+        <BlockLinkImage
+          class="w-col6 mx-auto"
+          link="https://www.nokidsinprison.org/experience/"
+          image="/about/nkip-digital.jpg"
+          text="#NKIP Digital Experience"
+          color="var(--nkip)"
+        />
+      </div>
+
+      <BlockTextHeading class="mt-8" text="Colophon" />
+
+      <div class="mt-12 pb-8 mx-12">
+        <div class="ml-col3 mr-col3 px-8 columns-3 gap-8">
           <div class="break-after-column text-runde-xs break-inside-avoid">
             <h2 class="uppercase font-bold mb-4">Website Design</h2>
              <a href="https://jasonkillinger.com/" target="_blank" class="underline hover:text-navigation-highlight">Jason Killinger</a> & <br> <a href="https://r-s-s.net" target="_blank" class="underline hover:text-navigation-highlight">Rahul Subhash Shinde</a>
